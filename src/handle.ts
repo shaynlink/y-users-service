9784dc30c248d7ee1df3486abe1974c81d08fd9d
adrien morand <adrie@CJT5FY3.be-ad.loc>
2024-04-22 15:55:10 +0200

    add users routs

--- src/handle.ts
+++ src/handle.ts
@@ -3,15 +3,17 @@ import Route from 'codebase/src/Route';
 
 export function setUpHandle(handle: HTTPHandle) {
   handle.initiateHealthCheckRoute();
-  handle.createRoute('/', (route: Route) => {
+  handle.app.locals.users = handle.app.locals.database.collection('users');
+
+  handle.createRoute('/users', (route: Route) => {
     // Set middleware for each route endpoints
-    route.setGlobalMiddleware('(/) logger', (req, res, next) => {
-      console.log('Middleware for /');
+    route.setGlobalMiddleware('(/users) logger', (req, res, next) => {
+      console.log('Middleware for /users');
       next();
     })
 
     // Creare a new middleware
-    const authentification = route.createMiddleware('(/) authentification', (req, res, next) => {
+    const authentification = route.createMiddleware('(/users) authentification', (req, res, next) => {
       if (req.headers.authorization !== 'Bearer token') {
         return res.status(200).send(handle.createResponse(req, res, null, new ErrorResponse('Unauthorized', 403)));
       }
@@ -26,12 +28,116 @@ export function setUpHandle(handle: HTTPHandle) {
     route.mapper.use(authentification);
 
     route.mapper.get(
-      '/',
+      '/me',
       authentification,
       (req, res) => {
         return handle.createResponse(req, res, {
-          message: 'Hello ' + res.locals.user.name
+          message: res.locals.users.findOne({ id: res.locals.user.id })
         }, null);
+      });
+
+    route.mapper.patch(
+      '/me',
+      authentification,
+      (req, res) => {
+        const user = res.locals.users.findOne({ id: res.locals.user.id });
+        const { name } = req.body;
+
+        if (!name) {
+          return handle.createResponse(req, res, null, new ErrorResponse('Name is required', 400));
+        }
+
+        user.name = name;
+        res.locals.users.updateOne({ id: res.locals.user.id }, user);
+
+        return handle.createResponse(req, res, {
+          message: user
+        }, null);
+      });
+
+    route.mapper.get(
+      '/me/following',
+      authentification,
+      (req, res) => {
+        return handle.createResponse(req, res, {
+          message: res.locals.users.findOne({ id: res.locals.user.id }).following
+        }, null);
+      });
+
+    route.mapper.get(
+      '/me/followers',
+      authentification,
+      (req, res) => {
+        return handle.createResponse(req, res, {
+          message: res.locals.users.findOne({ id: res.locals.user.id }).followers
+        }, null);
+      });
+
+    route.mapper.get(
+      '/:id',
+      authentification,
+      (req, res) => {
+        return handle.createResponse(req, res, {
+          message: res.locals.users.findOne({ id: req.params.id })
+        }, null);
+      });
+
+    route.mapper.get(
+      '/:id/following',
+      authentification,
+      (req, res) => {
+        return handle.createResponse(req, res, {
+          message: res.locals.users.findOne({ id: req.params.id }).following
+        }, null);
+      });
+    
+    route.mapper.get(
+      '/:id/followers',
+      authentification,
+      (req, res) => {
+        return handle.createResponse(req, res, {
+          message: res.locals.users.findOne({ id: req.params.id }).followers
+        }, null);
+      });
+
+    route.mapper.put(
+      '/:id/follow',
+      authentification,
+      (req, res) => {
+        const user = res.locals.users.findOne({ id: req.params.id });
+        const me = res.locals.users.findOne({ id: res.locals.user.id });
+
+        if (user.followers.includes(me.id)) {
+          return handle.createResponse(req, res, null, new ErrorResponse('Already following this user', 400));
+        }
+
+        user.followers.push(me.id);
+        me.following.push(user.id);
+
+        res.locals.users.updateOne({ id: req.params.id }, user);
+        res.locals.users.updateOne({ id: res.locals.user.id }, me);
+
+        return handle.createResponse(req, res, null, null);
+      });
+    
+    route.mapper.delete(
+      '/:id/follow',
+      authentification,
+      (req, res) => {
+        const user = res.locals.users.findOne({ id: req.params.id });
+        const me = res.locals.users.findOne({ id: res.locals.user.id });
+
+        if (!user.followers.includes(me.id)) {
+          return handle.createResponse(req, res, null, new ErrorResponse('Not following this user', 400));
+        }
+
+        user.followers = user.followers.filter((id: number) => id !== me.id);
+        me.following = me.following.filter((id: number) => id !== user.id);
+
+        res.locals.users.updateOne({ id: req.params.id }, user);
+        res.locals.users.updateOne({ id: res.locals.user.id }, me);
+
+        return handle.createResponse(req, res, null, null);
       });
   })
 }
